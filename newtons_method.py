0f0df962619dab3c9b19c56f638fd2beea1dbdb2
Kennedy Mindeman <=>
2023-11-14 16:45:41 -0600

    Add function and term class and diff function
    Add functions and classes that will allow for newtons method to be used on a
    polynomial function without taking a derivative before calling the
    function

--- newtons_method.py
+++ newtons_method.py
@@ -1,2 +1,27 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Term:
+    coefficient: float
+    power: float
+
+
+@dataclass
+class Function:
+    terms: list[Term]
+
+
 def iterate_root_guess(guess, function, derivative):
     return guess - function(guess) / derivative(guess)
+
+
+def differentiate(function: Function):
+    def diff_term(term: Term):
+        if term.power == 0:
+            raise NotImplementedError
+
+        return Term(term.coefficient * term.power, term.power - 1)
+
+    terms = [diff_term(term) for term in function.terms if term.power != 0]
+    return Function(terms)
