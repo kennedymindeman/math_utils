3bc810deceed7d22b58d88a6c377fca4fff86b95
Kennedy Mindeman <kennedymindeman@gmail.com>
2024-03-06 00:55:20 -0600

    Add change newtons method functions to work with new format

--- newtons_method.py
+++ newtons_method.py
@@ -1,15 +1,4 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Term:
-    coefficient: float
-    power: float
-
-
-@dataclass
-class Function:
-    terms: list[Term]
+from functions import Function
 
 
 def newtons_method(guess, function, num_iterations):
@@ -19,26 +8,8 @@ def newtons_method(guess, function, num_iterations):
     return guess
 
 
-def iterate_root_guess(guess, function):
-    derivative = differentiate(function)
-    value_at_guess = evaluate(guess, function)
-    slope_at_guess = evaluate(guess, derivative)
+def iterate_root_guess(guess, function: Function):
+    derivative = function.differentiate()
+    value_at_guess = function.evaluate(guess)
+    slope_at_guess = derivative.evaluate(guess)
     return guess - value_at_guess / slope_at_guess
-
-
-def differentiate(function: Function):
-    def diff_term(term: Term):
-        if term.power == 0:
-            raise NotImplementedError
-
-        return Term(term.coefficient * term.power, term.power - 1)
-
-    terms = [diff_term(term) for term in function.terms if term.power != 0]
-    return Function(terms)
-
-
-def evaluate(x_input, function: Function):
-    def evaluate_term(term: Term):
-        return term.coefficient * (x_input**term.power)
-
-    return sum(evaluate_term(term) for term in function.terms)
