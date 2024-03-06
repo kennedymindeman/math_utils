7e644d68910e320360e13026b66a96000d2f587b
Kennedy Mindeman <kennedymindeman@gmail.com>
2024-03-06 01:07:21 -0600

    Fix formatting for oop

--- test/test_newtons_method.py
+++ test/test_newtons_method.py
@@ -1,23 +1,20 @@
 from armstrong import is_armstrong_number
-from newtons_method import (
-    Function,
-    Term,
-    differentiate,
-    iterate_root_guess,
-    newtons_method,
-)
+from functions import PolynomialTerm
+from functions import Function
+from newtons_method import iterate_root_guess, newtons_method
 
 
 def test_differentiate():
-    function = Function([Term(1, 3), Term(-1, 1), Term(1, 0)])
-    derivative = Function([Term(3, 2), Term(-1, 0)])
-    assert derivative == differentiate(function)
+    tups = tuple([PolynomialTerm(1, 3), PolynomialTerm(-1, 1), PolynomialTerm(1, 0)])
+    function = Function(tups)
+    derivative = Function(tuple([PolynomialTerm(3, 2), PolynomialTerm(-1, 0)]))
+    assert derivative == function.differentiate()
 
 
 def test_iterate_root_guess():
-    function = Function([Term(1, 3), Term(-1, 1), Term(1, 0)])
+    function = Function(tuple([PolynomialTerm(1, 3), PolynomialTerm(-1, 1), PolynomialTerm(1, 0)]))
     guess = -1
-    guess = iterate_root_guess(guess, function)
+    guess = (guess, function)
     assert guess == -1.5
     guess = iterate_root_guess(guess, function)
     assert guess == -1.3478260869565217
@@ -26,7 +23,7 @@ def test_iterate_root_guess():
 
 
 def test_newtons_method():
-    function = Function([Term(1, 3), Term(-1, 1), Term(1, 0)])
+    function = Function(tuple([PolynomialTerm(1, 3), PolynomialTerm(-1, 1), PolynomialTerm(1, 0)]))
     guess = -1
     root_approx = newtons_method(guess, function, 3)
     assert root_approx == -1.325200398950907
